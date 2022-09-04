bb03a9ce58a188a5418c4a1de8ebd9a66270df18
Akshay <53929892+badcoderxd@users.noreply.github.com>
2022-09-05 02:32:07 +0530

    Update fileUploadController.js

--- server/src/Controller/Common/fileUploadController.js
+++ server/src/Controller/Common/fileUploadController.js
@@ -14,10 +14,11 @@ exports.uploadSingleFile = async (take, give) => {
         //let file = await take.file();
        // let uploaded = await upload.single("files");
         let RemoteUploadServiceRef = new RemoteUploadService();
-
-        const url = await RemoteUploadServiceRef.uploadToCloud(take.file.path)
+        console.log(take.file,"file")
+        const url = await RemoteUploadServiceRef.uploadToCloud(take.file.path);
+        console.log(url);
         console.log(take.file,"hello")
-        await setTimeout(async() => {await unlinkAsync(take.file.path)}, 50000 ) 
+        await setTimeout(async() => {await unlinkAsync(take.file.path)}, 1000 ) 
         return {path:url,old_path:take.file.path};
     } catch (err) {
       throw boom.boomify(err);
